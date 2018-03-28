d1c953a0d80308a40a862aea7ab2c86107f448d6
Jason Cosman <Jason Cosman>
2018-03-28 07:42:07 -0300

    Add project files.

---
+++ Xrm.Sports.Plugins/P - EndGame.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using Microsoft.Xrm.Sdk.Workflow;
+using Microsoft.Crm.Sdk.Messages;
+
+namespace Xrm.Sports.Plugins
+{
+    public class EndGame : IPlugin
+
+    {
+
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            
+            
+            if(serviceProvider ==null)
+            {
+                throw new ArgumentNullException("serviceProvider", "serviceProvider cannot be a null refrence");
+            }
+
+            IPluginExecutionContext context = (IPluginExecutionContext)(serviceProvider.GetService(typeof(IPluginExecutionContext)));
+
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            //Lets get the game information and update each teams record
+            Entity target = (Entity)context.InputParameters["Target"];
+
+            try
+            {
+                Entity aGame = null;
+
+                tracingService.Trace("Let's get the game entity with all of it's columns. The ID is:" + target.Id.ToString());
+                if(context.MessageName== "Update")
+                {
+                    aGame = (Entity)service.Retrieve(target.LogicalName, target.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));                
+                }
+                else
+                {
+                    throw new Exception("Plugin:EndGame - This plugin should only be executed against an update message not a create or delete.");
+                }
+
+                //Let's validate that it returned something
+                if(aGame != null)
+                {
+                    EntityReference homeTeam = null;
+                    if(aGame.Attributes.Contains("jmvp_hometeam"))
+                    {
+                        homeTeam = (EntityReference)aGame.Attributes["jmvp_hometeam"];
+                    }
+
+                    EntityReference awayTeam = null;
+                    if (aGame.Attributes.Contains("jmvp_awayteam"))
+                    {
+                        awayTeam = (EntityReference)aGame.Attributes["jmvp_awayteam"];
+                    }
+
+                    OptionSetValue gameOutcome = null;
+                    if (aGame.Attributes.Contains("jmvp_outcome"))
+                    {
+                        gameOutcome = (OptionSetValue)aGame.Attributes["jmvp_outcome"];
+                        //Home - 492,470,000
+                        //Away - 492,470,001
+                        //Tie  - 492,470,002
+                    }
+
+                    Entity homeTeamEntity = getEntitywithFields(homeTeam, true, service);
+
+                    Entity awayTeamEntity = getEntitywithFields(awayTeam, true, service);
+
+                    switch(gameOutcome.Value)
+                    {
+                        //Home
+                        case 492470000:
+                            homeTeamEntity.Attributes["jmvp_winrecord"] = (int)homeTeamEntity.Attributes["jmvp_winrecord"] + 1;
+                            homeTeamEntity.Attributes["jmvp_totalpoints"] = (int)homeTeamEntity.Attributes["jmvp_totalpoints"] + 2;
+                            awayTeamEntity.Attributes["jmvp_lossrecord"] = (int)awayTeamEntity.Attributes["jmvp_lossrecord"] + 1;
+                            service.Update(homeTeamEntity);
+                            service.Update(awayTeamEntity);
+                            break;
+                        //Away
+                        case 492470001:
+                            awayTeamEntity.Attributes["jmvp_winrecord"] = (int)awayTeamEntity.Attributes["jmvp_winrecord"] + 1;
+                            awayTeamEntity.Attributes["jmvp_totalpoints"] = (int)awayTeamEntity.Attributes["jmvp_totalpoints"] + 2;
+                            homeTeamEntity.Attributes["jmvp_lossrecord"] = (int)homeTeamEntity.Attributes["jmvp_lossrecord"] + 1;
+
+                            service.Update(awayTeamEntity);
+                            service.Update(homeTeamEntity);
+                            break;
+                        //Tie
+                        case 492470002:
+                            
+                            if(homeTeamEntity.Attributes.Contains("jmvp_tierecord"))
+                            { 
+                                homeTeamEntity.Attributes["jmvp_tierecord"] = (int)homeTeamEntity.Attributes["jmvp_tierecord"] + 1;
+
+                                service.Update(homeTeamEntity);
+                            }
+                            else
+                            {
+                                //homeTeamEntity.Attributes.Add(new KeyValuePair<string, object>("jmvp_tierecord"), (int)homeTeamEntity.Attributes["jmvp_tierecord"] + 1));
+                            }
+                            if (homeTeamEntity.Attributes.Contains("jmvp_tierecord"))
+                            {
+                                awayTeamEntity.Attributes["jmvp_tierecord"] = (int)awayTeamEntity.Attributes["jmvp_tierecord"] + 1;
+                                service.Update(awayTeamEntity);
+                            }
+                            else
+                            {
+
+                            }
+
+
+                            break;
+
+
+                    }
+
+
+                }
+
+
+
+            }
+            catch(Exception ex)
+            {
+                throw (ex);
+            }
+
+        }
+
+        private Entity getEntitywithFields(EntityReference entityToRetrieve, Boolean columnSet, IOrganizationService service)
+        {
+
+            return service.Retrieve(entityToRetrieve.LogicalName, entityToRetrieve.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet(columnSet));
+
+
+        }
+    }
+}
+
---
+++ Xrm.Sports.PluginsTests/P - EndGameTests.cs
@@ -0,0 +1,319 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xrm.Sports.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FakeXrmEasy;
+using Microsoft.Xrm.Sdk;
+
+namespace Xrm.Sports.Plugins.Tests
+{
+    [TestClass()]
+    public class EndGameTests
+    {
+        [TestMethod()]
+        public void HomeTeamWins()
+        {
+            
+
+            var fakedContext = new XrmFakedContext();
+
+            var team1 = new Entity("jmvp_sportsteam");
+            team1.Id = Guid.NewGuid();
+            team1["jmvp_name"] = "Canadiens";
+            team1["jmvp_winrecord"] = 10;
+            team1["jmvp_lossrecord"] = 8;
+            team1["jmvp_totalpoints"] = 20;
+
+            var team2 = new Entity("jmvp_sportsteam");
+            team2.Id = Guid.NewGuid();
+            team2["jmvp_name"] = "Maple Leafs";
+            team2["jmvp_winrecord"] = 12;
+            team2["jmvp_lossrecord"] = 6;
+            team2["jmvp_totalpoints"] = 24;
+
+            var game1 = new Entity("jvmp_game");
+            game1.Id = Guid.NewGuid();
+            game1["jvmp_name"] = "November 5th - Canadiens vs Maple Leafs";
+            game1["jmvp_hometeam"] = new EntityReference(team1.LogicalName, team1.Id);
+            game1["jmvp_awayteam"] = new EntityReference(team2.LogicalName, team2.Id);
+            game1["jmvp_outcome"] = new OptionSetValue(492470000);
+
+            var game2 = new Entity("jvmp_game");
+            game2.Id = Guid.NewGuid();
+            game2["jvmp_name"] = "December 15th - Maple Leafs vs Canadiens";
+            game2["jmvp_hometeam"] = new EntityReference(team2.LogicalName, team2.Id);
+            game2["jmvp_awayteam"] = new EntityReference(team1.LogicalName, team1.Id);
+            game2["jmvp_outcome"] = new OptionSetValue(492470001);
+
+
+            fakedContext.Initialize(new List<Entity>()
+            {
+                team1, team2, game1, game2
+            });
+
+
+            
+
+            ParameterCollection inputParameters = new ParameterCollection();
+            inputParameters.Add("Target", game1);
+
+            var plugCtx = fakedContext.GetDefaultPluginContext();
+            plugCtx.MessageName = "Update";
+            plugCtx.InputParameters = inputParameters;
+            plugCtx.Depth = 1;
+
+            var FakedPlugin = fakedContext.ExecutePluginWith<EndGame>(plugCtx);
+
+            IOrganizationService service = fakedContext.GetOrganizationService();
+
+            Entity updatedHomeTeam = service.Retrieve(team1.LogicalName, team1.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
+
+            Entity updatedAwayTeam = service.Retrieve(team2.LogicalName, team2.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(((int)team1["jmvp_winrecord"] + 1), (int)updatedHomeTeam["jmvp_winrecord"]);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(((int)team1["jmvp_totalpoints"] + 2), (int)updatedHomeTeam["jmvp_totalpoints"]);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(((int)team2["jmvp_lossrecord"] + 1), (int)updatedAwayTeam["jmvp_lossrecord"]);
+
+
+        }
+
+        [TestMethod()]
+        public void AwayTeamWins()
+        {
+
+
+            var fakedContext = new XrmFakedContext();
+
+            var team1 = new Entity("jmvp_sportsteam");
+            team1.Id = Guid.NewGuid();
+            team1["jmvp_name"] = "Canadiens";
+            team1["jmvp_winrecord"] = 10;
+            team1["jmvp_lossrecord"] = 8;
+            team1["jmvp_totalpoints"] = 20;
+
+            var team2 = new Entity("jmvp_sportsteam");
+            team2.Id = Guid.NewGuid();
+            team2["jmvp_name"] = "Maple Leafs";
+            team2["jmvp_winrecord"] = 12;
+            team2["jmvp_lossrecord"] = 6;
+            team2["jmvp_totalpoints"] = 24;
+
+            var game1 = new Entity("jvmp_game");
+            game1.Id = Guid.NewGuid();
+            game1["jvmp_name"] = "November 5th - Canadiens vs Maple Leafs";
+            game1["jmvp_hometeam"] = new EntityReference(team1.LogicalName, team1.Id);
+            game1["jmvp_awayteam"] = new EntityReference(team2.LogicalName, team2.Id);
+            game1["jmvp_outcome"] = new OptionSetValue(492470000);
+
+            var game2 = new Entity("jvmp_game");
+            game2.Id = Guid.NewGuid();
+            game2["jvmp_name"] = "December 15th - Maple Leafs vs Canadiens";
+            game2["jmvp_hometeam"] = new EntityReference(team2.LogicalName, team2.Id);
+            game2["jmvp_awayteam"] = new EntityReference(team1.LogicalName, team1.Id);
+            game2["jmvp_outcome"] = new OptionSetValue(492470001);
+
+
+            fakedContext.Initialize(new List<Entity>()
+            {
+                team1, team2, game1, game2
+            });
+
+
+
+
+            ParameterCollection inputParameters = new ParameterCollection();
+            inputParameters.Add("Target", game2);
+
+            var plugCtx = fakedContext.GetDefaultPluginContext();
+            plugCtx.MessageName = "Update";
+            plugCtx.InputParameters = inputParameters;
+            plugCtx.Depth = 1;
+
+
+
+            var FakedPlugin = fakedContext.ExecutePluginWith<EndGame>(plugCtx);
+
+
+            IOrganizationService service = fakedContext.GetOrganizationService();
+
+            Entity updatedAwayTeam = service.Retrieve(team1.LogicalName, team1.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
+
+            Entity updatedHomeTeam = service.Retrieve(team2.LogicalName, team2.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(((int)team1["jmvp_winrecord"] + 1), (int)updatedAwayTeam["jmvp_winrecord"]);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(((int)team1["jmvp_totalpoints"] + 2), (int)updatedAwayTeam["jmvp_totalpoints"]);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(((int)team2["jmvp_lossrecord"] + 1), (int)updatedHomeTeam["jmvp_lossrecord"]);
+
+
+        }
+
+
+        [TestMethod()]
+        public void Tie()
+        {
+
+
+            var fakedContext = new XrmFakedContext();
+
+            var team1 = new Entity("jmvp_sportsteam");
+            team1.Id = Guid.NewGuid();
+            team1["jmvp_name"] = "Canadiens";
+            team1["jmvp_winrecord"] = 10;
+            team1["jmvp_lossrecord"] = 8;
+            team1["jmvp_totalpoints"] = 20;
+            team1["jmvp_tierecord"] = 1;
+
+            var team2 = new Entity("jmvp_sportsteam");
+            team2.Id = Guid.NewGuid();
+            team2["jmvp_name"] = "Maple Leafs";
+            team2["jmvp_winrecord"] = 12;
+            team2["jmvp_lossrecord"] = 6;
+            team2["jmvp_totalpoints"] = 24;
+            team2["jmvp_tierecord"] = 1;
+
+
+            var game1 = new Entity("jvmp_game");
+            game1.Id = Guid.NewGuid();
+            game1["jvmp_name"] = "November 5th - Canadiens vs Maple Leafs";
+            game1["jmvp_hometeam"] = new EntityReference(team1.LogicalName, team1.Id);
+            game1["jmvp_awayteam"] = new EntityReference(team2.LogicalName, team2.Id);
+            game1["jmvp_outcome"] = new OptionSetValue(492470000);
+
+            var game2 = new Entity("jvmp_game");
+            game2.Id = Guid.NewGuid();
+            game2["jvmp_name"] = "December 15th - Maple Leafs vs Canadiens";
+            game2["jmvp_hometeam"] = new EntityReference(team2.LogicalName, team2.Id);
+            game2["jmvp_awayteam"] = new EntityReference(team1.LogicalName, team1.Id);
+            game2["jmvp_outcome"] = new OptionSetValue(492470001);
+
+            var game3 = new Entity("jvmp_game");
+            game3.Id = Guid.NewGuid();
+            game3["jvmp_name"] = "January 15th - Maple Leafs vs Canadiens";
+            game3["jmvp_hometeam"] = new EntityReference(team2.LogicalName, team2.Id);
+            game3["jmvp_awayteam"] = new EntityReference(team1.LogicalName, team1.Id);
+            game3["jmvp_outcome"] = new OptionSetValue(492470002);
+
+            fakedContext.Initialize(new List<Entity>()
+            {
+                team1, team2, game1, game2,game3
+            });
+
+
+
+
+            ParameterCollection inputParameters = new ParameterCollection();
+            inputParameters.Add("Target", game3);
+
+            var plugCtx = fakedContext.GetDefaultPluginContext();
+            plugCtx.MessageName = "Update";
+            plugCtx.InputParameters = inputParameters;
+            plugCtx.Depth = 1;
+
+
+
+            var FakedPlugin = fakedContext.ExecutePluginWith<EndGame>(plugCtx);
+
+
+            IOrganizationService service = fakedContext.GetOrganizationService();
+
+            Entity updatedAwayTeam = service.Retrieve(team1.LogicalName, team1.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
+
+            Entity updatedHomeTeam = service.Retrieve(team2.LogicalName, team2.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(((int)team1["jmvp_tierecord"] + 1), (int)updatedAwayTeam["jmvp_tierecord"]);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(((int)team2["jmvp_tierecord"] + 1), (int)updatedHomeTeam["jmvp_tierecord"]);
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private XrmFakedContext getFullFakedContext()
+        {
+            var fakedContext = new XrmFakedContext();
+
+
+            var team1 = new Entity("jmvp_sportsteam");
+            team1.Id = Guid.NewGuid();
+            team1["jmvp_name"] = "Canadiens";
+            team1["jmvp_winrecord"] = 10;
+            team1["jmvp_lossrecord"] = 8;
+            team1["jmvp_totalpoints"] = 20;
+
+            var team2 = new Entity("jmvp_sportsteam");
+            team2.Id = Guid.NewGuid();
+            team2["jmvp_name"] = "Maple Leafs";
+            team2["jmvp_winrecord"] = 12;
+            team2["jmvp_lossrecord"] = 6;
+            team2["jmvp_totalpoints"] = 24;
+
+            var game1 = new Entity("jvmp_game");
+            game1.Id = Guid.NewGuid();
+            game1["jvmp_name"] = "November 5th - Canadiens vs Maple Leafs";
+            game1["jmvp_hometeam"] = new EntityReference(team1.LogicalName, team1.Id);
+            game1["jmvp_awayteam"] = new EntityReference(team2.LogicalName, team1.Id);
+            game1["jmvp_outcome"] = new OptionSetValue(492470000);
+
+            var game2 = new Entity("jvmp_game");
+            game2.Id = Guid.NewGuid();
+            game2["jvmp_name"] = "December 15th - Maple Leafs vs Canadiens";
+            game2["jmvp_hometeam"] = new EntityReference(team2.LogicalName, team1.Id);
+            game2["jmvp_awayteam"] = new EntityReference(team1.LogicalName, team1.Id);
+            game2["jmvp_outcome"] = new OptionSetValue(492470001);
+
+
+            fakedContext.Initialize(new List<Entity>()
+            {
+                team1, team2, game1, game2
+            });
+
+            //"ajg_winrecord, ajg_lossrecord, ajg_points, ajg_tierecord"
+
+
+
+
+            return fakedContext;
+        }
+
+    }
+}
